2369e7932356ba918f39228360a29d6741b85144
mohamad <mohamad.borzoyi@gmail.com>
2022-04-26 08:38:00 +0430

    new lang file for pemission commands

---
+++ src/lang/routes.php
@@ -0,0 +1,140 @@
+<?php
+return [
+    'accepted' => [
+        'users' => [
+            'single' => 'کاربر',
+            'many' => 'کاربران',
+        ],
+        'roles' => [
+            'single' => 'نقش',
+            'many' => 'نقش ها',
+        ],
+        'brands' => [
+            'single' => 'برند',
+            'many' => 'برندها',
+        ],
+        'categories' => [
+            'single' => 'دسته بندی',
+            'many' => 'دسته بندی ها',
+        ],
+        'packages' => [
+            'single' => 'پکیج',
+            'many' => 'پکیج ها',
+        ],
+        'authentication_requests' => [
+            'single' => 'درخواست تدریس',
+            'many' => 'درخواست های تدریس',
+        ],
+        'settlement_requests' => [
+            'single' => 'درخواست تسویه حساب',
+            'many' => 'درخواست های تسویه حساب',
+        ],
+        'teachers' => [
+            'single' => 'مدرس',
+            'many' => 'مدرسین',
+        ],
+        'courses' => [
+            'single' => 'دوره',
+            'many' => 'دوره های آموزشی',
+        ],
+        'specialties' => [
+            'single' => 'تخصص',
+            'many' => 'تخصص ها',
+        ],
+        'discount_codes' => [
+            'single' => 'کد تخفیف',
+            'many' => 'کدهای تخفیف',
+        ],
+        'orders' => [
+            'single' => 'سفارش',
+            'many' => 'سفارشات',
+        ],
+        'sections' => [
+            'single' => 'بخش',
+            'many' => 'بخش ها',
+        ],
+        'videos' => [
+            'single' => 'ویدئو',
+            'many' => 'ویدئو ها',
+        ],
+        'photos' => [
+            'single' => 'تصویر',
+            'many' => 'تصاویر',
+        ],
+        'lessons' => [
+            'single' => 'جلسه',
+            'many' => 'جلسات',
+        ],
+        'products' => [
+            'single' => 'محصول',
+            'many' => 'محصولات',
+        ],
+        'articles' => [
+            'single' => 'مقاله',
+            'many' => 'مقالات',
+        ],
+        'permissions' => 'دسترسی های',
+        'files' => [
+            'single' => 'فایل',
+            'many' => 'فایل ها',
+        ],
+        'managers' => [
+            'single' => 'مدیر',
+            'many' => 'مدیران',
+        ],
+        'edit' => 'ویرایش',
+        'update' => 'ویرایش',
+        'status' => 'وضعیت',
+        'create' => 'ایجاد',
+        'upload' => [
+            'many' => 'آپلود',
+            'single' => 'آپلود',
+            'clear_method' => ['POST'],
+        ],
+        'reorder' => [
+            'many' => 'مرتب سازی مجدد',
+            'single' => 'مرتب سازی مجدد',
+            'clear_method' => ['POST'],
+        ],
+        'load' => [
+            'many' => 'بارگذاری',
+            'single' => 'بارگذاری',
+            'clear_method' => ['POST'],
+        ],
+        'ajax' => [
+            'many' => 'ایجکس',
+            'single' => 'ایجکس',
+            'clear_method' => ['POST'],
+        ],
+        'download' => [
+            'many' => 'دانلود',
+            'single' => 'دانلود',
+        ],
+        'awaiting_review' => [
+            'many' => 'درانتظار بررسی',
+            'single' => 'درانتظار بررسی',
+        ],
+        'dashboard' => 'داشبورد',
+        'view' => 'نمایش',
+        'list' => 'لیست',
+    ],
+
+    'blocked' => [
+        'update',
+        'login',
+        'logout',
+    ],
+
+    'methods' => [
+        'POST' => 'افزودن',
+        'GET' => 'صفحه',
+        'PUT' => 'ویرایش',
+        'PATCH' => 'ویرایش',
+        'DELETE' => 'حذف',
+    ],
+
+    'specials' => [
+        '{',
+        '}',
+    ],
+];
